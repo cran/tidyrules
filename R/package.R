d5de3f799c786b042e338f078fc289933304c5b6
Srikanth Komala Sheshachala <sri.teach@gmail.com>
2024-06-29 22:40:06 +0000

    version 0.2.7

--- R/package.R
+++ R/package.R
@@ -4,12 +4,97 @@
 ################################################################################
 
 #' @name package_tidyrules
-#' @title About 'tidyrules' package
-#' @description Obtain rules as tidy dataframes
-
+#' @title `tidyrules`
+#' @description `tidyrules` package provides a framework to work with decision
+#'   rules. Rules can be extracted from supported models using [tidy], augmented
+#'   using validation data by [augment][augment.rulelist], manipulated using
+#'   standard dataframe operations, (modified) rulelists can be used to
+#'   [predict][predict.rulelist] on unseen (test) data. Utilities include:
+#'   Create a rulelist manually ([as_rulelist][as_rulelist.data.frame]), Export
+#'   a rulelist to SQL ([to_sql_case]) and so on. The package offers two
+#'   classes; [rulelist] and [ruleset] based on dataframe.
+#' @seealso [rulelist], [tidy], [augment][augment.rulelist],
+#'   [predict][predict.rulelist]
 #' @importFrom magrittr %>%
+#' @importFrom rlang %||%
+#' @importFrom data.table :=
+#' @importFrom utils data
+#' @importFrom stats IQR
+#' @importFrom stats weighted.mean
+#' @importFrom tidytable select
+#' @importFrom tidytable arrange
+#' @importFrom tidytable mutate
+#' @importFrom tidytable summarise
+#' @importFrom tidytable distinct
+#' @importFrom tidytable all_of
+#' @importFrom tidytable n
+#' @importFrom tidytable left_join
+#' @importFrom tidytable right_join
+#' @importFrom tidytable inner_join
+#' @importFrom tidytable inner_join
+#' @importFrom tidytable nest
+#' @importFrom tidytable unnest
+#' @importFrom tidytable row_number
+#' @importFrom tidytable drop_na
+#' @importFrom tidytable relocate
+#' @importFrom tidytable bind_rows
+#' @importFrom tidytable pull
+#' @importFrom tidytable slice
+#' @importFrom tidytable pivot_wider
+#' @importFrom tidytable pivot_longer
+#' @importFrom tidytable bind_cols
+#' @importFrom tidytable across
+#' @importFrom graphics abline
+#' @importFrom graphics axis
+#' @importFrom graphics legend
+#' @importFrom graphics lines
+#' @importFrom stats runif
+#' @importFrom utils head
+#' @importFrom utils tail
+#' @importFrom utils capture.output
+#'
 "_PACKAGE"
 
-is.integerish <- getFromNamespace("is.integerish", "assertthat")
+list.rules.party = getFromNamespace(".list.rules.party", "partykit")
 
-list.rules.party <- getFromNamespace(".list.rules.party", "partykit")
+utils::globalVariables(c(".",
+                         "LHS",
+                         "RHS",
+                         "committee",
+                         "desc",
+                         "dev",
+                         "lift",
+                         "n",
+                         "predict_class",
+                         "rule_nbr",
+                         "rule_number",
+                         "support",
+                         "trial_number",
+                         "yval",
+                         "confidence",
+                         "rn__",
+                         "row_nbr",
+                         "pref__",
+                         "data",
+                         "weight",
+                         "response",
+                         "terminal_node_id",
+                         "sum_weight",
+                         "prevalence",
+                         "winning_response",
+                         "average",
+                         "RMSE",
+                         "weight__",
+                         "prevalence_0",
+                         ".data",
+                         "rn_df",
+                         "trial_nbr",
+                         "error",
+                         "data__",
+                         "rn_df__",
+                         "hit",
+                         "priority",
+                         "value"
+                         )
+                       )
+
--- R/utils.R
+++ R/utils.R
@@ -1,35 +1,36 @@
-# utils
-################################################################################
+#*******************************************************************************
 # This is the part of the 'tidyrules' R package hosted at
 # https://github.com/talegari/tidyrules with GPL-3 license.
-################################################################################
+#*******************************************************************************
 
+#' @keywords internal
 #' @name positionSpaceOutsideSinglequotes
 #' @title Position of space outside single quotes
 #' @description (vectorised) Detect the position of space in a string not within
 #'   a pair of single quotes
-#' @param string A chracter vector
+#' @param string A character vector
 #' @return A integer vector of positions
 #' @examples
 #' \donttest{
 #' tidyrules:::positionSpaceOutsideSinglequotes(c("hello", "hel' 'o "))
 #' }
 #'
-positionSpaceOutsideSinglequotes <- Vectorize(
+positionSpaceOutsideSinglequotes = Vectorize(
   function(string){
 
-    assertthat::assert_that(assertthat::is.string(string))
+    checkmate::assert_string(string)
 
-    fullsplit               <- strsplit(string, "")[[1]]
-    is_singlequote          <- (fullsplit == "'")
-    parity_singlequote_left <- cumsum(fullsplit == "'") %% 2
-    space_position          <- which(fullsplit == " ")
+    fullsplit               = strsplit(string, "")[[1]]
+    is_singlequote          = (fullsplit == "'")
+    parity_singlequote_left = cumsum(fullsplit == "'") %% 2
+    space_position          = which(fullsplit == " ")
 
     space_position[parity_singlequote_left[space_position] == 0]
-  }
-  , USE.NAMES = FALSE
+  },
+  USE.NAMES = FALSE
 )
 
+#' @keywords internal
 #' @name removeEmptyLines
 #' @title Remove empty lines
 #' @description Remove empty strings from a character vector
@@ -40,10 +41,11 @@ positionSpaceOutsideSinglequotes <- Vectorize(
 #' tidyrules:::removeEmptyLines(c("abc", "", "d"))
 #' }
 #'
-removeEmptyLines <- function(strings){
+removeEmptyLines = function(strings){
   strings[!(strings == "")]
 }
 
+#' @keywords internal
 #' @name strSplitSingle
 #' @title String split a string
 #' @description and return a character vector (not a list)
@@ -55,19 +57,20 @@ removeEmptyLines <- function(strings){
 #' tidyrules:::strSplitSingle("abc,d", ",")
 #' }
 #'
-strSplitSingle <- function(string, pattern){
+strSplitSingle = function(string, pattern){
 
-  assertthat::assert_that(assertthat::is.string(string))
+  checkmate::assert_string(string)
   stringr::str_split(string, pattern)[[1]]
 
 }
 
+#' @keywords internal
 #' @name strHead
 #' @title Vectorized semantic equivalent of 'head' for a string
 #' @description Picks the substring starting from the first character
 #' @param string string
 #' @param n (integer) Number of characters
-#' @details 'n' can be in the interval [-len + 1, len] (both ends inclusive)
+#' @details 'n' can be in the interval \[-len + 1, len\] (both ends inclusive)
 #' @return A string
 #' @examples
 #' \donttest{
@@ -75,32 +78,31 @@ strSplitSingle <- function(string, pattern){
 #' tidyrules:::strHead(c("string", "string2"), -1)
 #' }
 #'
-strHead <- Vectorize(
+strHead = Vectorize(
   function(string, n){
 
-    assertthat::assert_that(assertthat::is.string(string))
-    len <- stringr::str_length(string)
-    assertthat::assert_that(is.integerish(n) && length(n) == 1 && n != 0
-                            , msg = "'n' should be an integer"
-                            )
+    checkmate::assert_string(string)
+
+    len = stringr::str_length(string)
+    checkmate::assert_integerish(n, len = 1)
+    checkmate::assert(n != 0)
     if(n < 0){
-      n <- len + n
+      n = len + n
     }
 
     return( stringr::str_sub(string, 1, n) )
-  }
-  , vectorize.args = "string"
-  , USE.NAMES = FALSE
+  },
+  vectorize.args = "string",
+  USE.NAMES = FALSE
   )
 
-
-
+#' @keywords internal
 #' @name strTail
 #' @title Vectorized semantic equivalent of tail for a string
 #' @description Picks the substring starting from the first character
 #' @param string string
 #' @param n (integer) Number of characters
-#' @details 'n' can be in the interval [-len + 1, len] (both ends inclusive)
+#' @details 'n' can be in the interval \[-len + 1, len\] (both ends inclusive)
 #' @return A string
 #' @examples
 #' \donttest{
@@ -108,23 +110,25 @@ strHead <- Vectorize(
 #' tidyrules:::strTail(c("string", "string2"), -1)
 #' }
 #'
-strTail <- Vectorize(
+strTail = Vectorize(
   function(string, n){
 
-    assertthat::assert_that(assertthat::is.string(string))
-    len <- stringr::str_length(string)
-    assertthat::assert_that(is.integerish(n) && length(n) == 1 && n != 0
-                            , msg = "'n' should be an integer"
-                            )
-    if(n < 0){
-      n <- len + n
+    checkmate::assert_string(string)
+
+    len = stringr::str_length(string)
+    checkmate::assert_integerish(n, len = 1)
+    checkmate::assert(n != 0)
+    if (n < 0){
+      n = len + n
     }
 
     return( stringr::str_sub(string, len - n + 1, len) )
-  }
-  , vectorize.args = "string"
-  , USE.NAMES = FALSE
+  },
+  vectorize.args = "string",
+  USE.NAMES = FALSE
   )
+
+#' @keywords internal
 #' @name addBackquotes
 #' @title Add backquotes
 #' @description (vectorized) Add backquotes when a string has a space in it
@@ -135,19 +139,22 @@ strTail <- Vectorize(
 #' tidyrules:::addBackquotes(c("ab", "a b"))
 #' }
 #'
-addBackquotes <- Vectorize(
+addBackquotes = Vectorize(
   function(string){
-    res <- string
-    if(stringr::str_count(string, "\\s") > 0){
+    checkmate::assert_string(string)
+
+    res = string
+    if (stringr::str_count(string, "\\s") > 0){
       if(strHead(string, 1) != "`" && strTail(string, 1) != "`"){
-          res <- stringr::str_c("`", string, "`")
+          res = stringr::str_c("`", string, "`")
       }
     }
     return(res)
-  }
-  , USE.NAMES = FALSE
+  },
+  USE.NAMES = FALSE
   )
 
+#' @keywords internal
 #' @name strReplaceReduce
 #' @title Sequential string replace
 #' @description Sequential string replace via reduce
@@ -160,18 +167,212 @@ addBackquotes <- Vectorize(
 #' tidyrules:::strReplaceReduce("abcd", c("ab", "dc"), c("cd", "ab"))
 #' }
 #'
-strReplaceReduce <- function(string, pattern, replacement){
+strReplaceReduce = function(string, pattern, replacement){
 
   stopifnot(length(pattern) == length(replacement))
-  io <- list(pattern, replacement) %>%
+  io =
+    list(pattern, replacement) %>%
     purrr::transpose() %>%
     purrr::map(unlist)
 
-  purrr::reduce(io
-                 , function(x, y) stringr::str_replace_all(x
-                                                           , y[[1]]
-                                                           , y[[2]]
-                                                           )
-                 , .init = string
-                 )
+  purrr::reduce(io,
+                function(x, y) stringr::str_replace_all(x, y[[1]], y[[2]]),
+                .init = string
+                )
+}
+
+#' @keywords internal
+#' @name varSpec
+#' @title Get variable specification for a Cubist/C5 object
+#' @description Obtain variable names, type (numeric, ordered, factor) and
+#'   levels as a tidytable
+#' @param object Cubist/C5 object
+#' @return A tidytable with three columns: variable(character), type(character)
+#'   and levels(a list-column). For numeric variables, levels are set to NA.
+#' @examples
+#' \dontrun{
+#' data("attrition", package = "modeldata")
+#' cols_att = setdiff(colnames(attrition), c("MonthlyIncome", "Attrition"))
+#'
+#' cb_att = Cubist::cubist(x = attrition[, cols_att],
+#'                         y = attrition[["MonthlyIncome"]]
+#'                         )
+#' varSpec(cb_att)
+#' }
+varSpec = function(object){
+
+  # 1. split ny newline
+  # 2. remove a few header lines
+  # 3. get variables and details
+
+  lines_raw =
+    object[["names"]] %>%
+    strSplitSingle("\\n")
+
+  outcome_line_number = stringr::str_which(lines_raw, "^outcome:")
+
+  lines =
+    lines_raw[-(1:outcome_line_number)] %>%
+    removeEmptyLines()
+
+  split_lines =
+    lines %>%
+    stringr::str_split(":") %>%
+    purrr::transpose()
+
+  variables =
+    split_lines %>%
+    magrittr::extract2(1) %>%
+    unlist() %>%
+    stringr::str_replace_all("\\\\", "") # clean up variable names
+
+  details =
+    split_lines %>%
+    magrittr::extract2(2) %>%
+    unlist() %>%
+    stringr::str_trim()
+
+  # handle a detail depending on its type
+  handleDetail = function(adetail){
+
+    if (adetail == "continuous."){
+      # handle numeric/integer
+      out = list(type = "numeric", levels = NA_character_)
+
+    } else if (stringr::str_detect(adetail, "^\\[ordered\\]")){
+      # handle ordered factors
+
+      levels =
+        adetail %>%
+        strSplitSingle("\\[ordered\\]") %>%
+        magrittr::extract(2) %>%
+        strHead(-1) %>%
+        strSplitSingle(",") %>%
+        stringr::str_trim()
+
+      out = list(type = "ordered", levels = levels)
+
+    } else { # handle unordered factors
+
+      levels =
+        adetail %>%
+        strHead(-1) %>%
+        strSplitSingle(",") %>%
+        stringr::str_trim()
+
+      out = list(type = "factor", levels = levels)
+    }
+
+  return(out)
+  }
+
+  details_cleaned =
+    details %>%
+    purrr::map(handleDetail) %>%
+    purrr::transpose()
+
+  details_cleaned[["type"]]     = unlist(details_cleaned[["type"]])
+  details_cleaned[["variable"]] = variables
+
+  res = tidytable::as_tidytable(details_cleaned)
+  return(res)
+}
+
+#' @name convert_rule_flavor
+#' @title Convert a R parsable rule to python/sql parsable rule
+#' @description Convert a R parsable rule to python/sql parsable rule
+#' @param rule (chr vector) R parsable rule(s)
+#' @param flavor (string) One among: 'python', 'sql'
+#' @return (chr vector) of rules
+#' @seealso [rulelist], [tidy], [augment][augment.rulelist], [predict][predict.rulelist], [to_sql_case]
+#' @family Auxiliary Rulelist Utility
+#' @export
+convert_rule_flavor = function(rule, flavor){
+
+  checkmate::assert_character(rule)
+  checkmate::assert_string(flavor)
+  flavor = stringr::str_to_lower(flavor)
+  checkmate::assert_choice(flavor, c("python", "sql"))
+
+  if (flavor == "python"){
+    res =
+      rule %>%
+      stringr::str_replace_all("\\( ", "") %>%
+      stringr::str_replace_all(" \\)", "") %>%
+
+      stringr::str_replace_all("%in%", "in") %>%
+      stringr::str_replace_all("c\\(", "[") %>%
+      stringr::str_replace_all("\\)", "]") %>%
+
+      stringr::str_replace_all("&", " ) and (") %>%
+
+      stringr::str_c("( ", ., " )") %>%
+      stringr::str_squish()
+
+  } else if (flavor == "sql"){
+    res =
+      rule %>%
+      stringr::str_replace_all("==", "=") %>%
+
+      stringr::str_replace_all("\\( ", "") %>%
+      stringr::str_replace_all(" \\)", "") %>%
+
+      stringr::str_replace_all("%in%", "IN") %>%
+      stringr::str_replace_all("c\\(", "(") %>%
+      stringr::str_replace_all("\\)", ")") %>%
+
+      stringr::str_replace_all("&", " ) AND (") %>%
+
+      stringr::str_c("( ", ., " )") %>%
+      stringr::str_squish()
+  }
+
+  attr(res, "flavor") = flavor
+  return(res)
+}
+
+#' @name to_sql_case
+#' @title Extract SQL case statement from a [rulelist]
+#' @description Extract SQL case statement from a [rulelist]
+#' @param rulelist A [rulelist] object
+#' @param rhs_column_name (string, default: "RHS") Name of the column in the
+#'   rulelist to be used as RHS (WHEN some_rule THEN rhs) in the sql case
+#'   statement
+#' @param output_colname (string, default: "output") Name of the output column
+#'   created by the SQL statement (used in case ... AS output_column)
+#' @return (string invisibly) SQL case statement
+#' @details As a side-effect, the SQL statement is cat to stdout. The output
+#' contains newline character.
+#' @examples
+#' model_c5 = C50::C5.0(Attrition ~., data = modeldata::attrition, rules = TRUE)
+#' tidy(model_c5)
+#' to_sql_case(tidy(model_c5))
+#' @seealso [rulelist], [tidy], [augment][augment.rulelist], [predict][predict.rulelist], [convert_rule_flavor]
+#' @family Auxiliary Rulelist Utility
+#' @export
+to_sql_case = function(rulelist,
+                       rhs_column_name = "RHS",
+                       output_colname = "output"
+                       ){
+
+  checkmate::assert_class(rulelist, "rulelist")
+  rhs_is_string = inherits(rulelist[[rhs_column_name]], c("character", "factor"))
+  lhs_sql = convert_rule_flavor(rulelist$LHS, flavor = "sql")
+  out = "CASE"
+
+  for (rn in seq_len(nrow(rulelist))) {
+
+    if (rhs_is_string) {
+      lhs = glue::glue("WHEN {lhs_sql[rn]} THEN '{rulelist[[rhs_column_name]][rn]}'")
+    } else {
+      lhs = glue::glue("WHEN {lhs_sql[rn]} THEN {rulelist[[rhs_column_name]][rn]}")
+    }
+    out = paste(out, lhs, sep = "\n")
+  }
+  out = paste(out, "ELSE NULL", sep = "\n")
+  out = paste(out, glue::glue("END AS {output_colname}"), sep = "\n")
+
+  cli::cli_code(out, language = "SQL")
+
+  return(invisible(out))
 }
